eecb27e5ab511ec60137abcddb7fdc827fbe75f9
Edgar Costa <edgarc@mit.edu>
2023-10-29 19:40:13 -0400

    the import is enough

--- pyfib/fib.pyx
+++ pyfib/fib.pyx
@@ -1,8 +1,12 @@
+# distutils: language=c
+# clang c
+
 from sage.rings.integer cimport Integer
 
 cpdef int fib(int n):
-    cdef Integer a = 0, b = 1
-    cdef Integer i
+    cdef int a = 0, b = 1
+    cdef int i
     for i in range(n):
         a, b = b, a+b
     return a
+
